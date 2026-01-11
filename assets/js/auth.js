65ba163f6e9cf9b319ccc9840558c64716f54acb
mahdimaaouch97-cpu <mahdimaaouch97@gmail.com>
2026-01-11 13:19:47 +0200

    Update auth.js

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,5 +1,3 @@
-// auth.js
-
 function login() {
     const username = document.getElementById("username").value.trim();
     const password = document.getElementById("password").value.trim();
@@ -10,11 +8,12 @@ function login() {
         return;
     }
 
-    // بيانات دخول بسيطة (لاحقًا يمكن ربطها بقاعدة بيانات)
     if (username === "admin" && password === "admin") {
         localStorage.setItem("loggedIn", "true");
         localStorage.setItem("user", username);
-        window.location.href = "index.html";
+
+        // مسار آمن
+        window.location.href = "/index.html";
     } else {
         error.innerText = "بيانات الدخول غير صحيحة";
     }
